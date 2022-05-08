0f6a9a1d274bc51a453865abfb76b25f3a81b58b
Aryansh <aryanshs@buffalo.edu>
2022-05-08 16:21:22 -0400

    name and age gui

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args){
         String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "hello "+ name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter Your Age"));
+        JOptionPane.showMessageDialog(null, "You are "+age+" years old");
     }
 }
